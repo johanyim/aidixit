6eef217104e4ba44bf73794621ab238d16803353
Jason Hui <jason903813@gmail.com>
2024-01-21 18:58:01 +0000

    get admin panel work

--- server/src/socketSetup.js
+++ server/src/socketSetup.js
@@ -1,4 +1,5 @@
 import { Server } from "socket.io"
+import { instrument } from "@socket.io/admin-ui"
 
 let io; // This will store the Socket.IO instance
 
@@ -7,22 +8,31 @@ function initializeSocket(server) {
         //cross origin resource request
         cors: {
             // origin: process.env.NODE_ENV === "production" ? false :
-            //     [
-            //         "http://localhost:8080",
-            //         "http://127.0.0.1:8080",
-            //         "http://localhost:3000",
-            //         "http://127.0.0.1:3000",
-            //         "https://admin.socket.io"
-            //     ]
-            origin: [ 
+                // [
+                //     "http://localhost:8080",
+                //     "http://127.0.0.1:8080",
+                //     "http://localhost:3000",
+                //     "http://127.0.0.1:3000",
+                //     "https://admin.socket.io",
+                // ]
+            origin: [
                 "http://localhost:8080", "http://127.0.0.1:8080",
                 "http://localhost:3000", "http://127.0.0.1:3000",
-            ]
+                "https://admin.socket.io",
+            ],
+            credentials: true,
         }
     })
 
+    instrument(io, {
+        auth: false,
+      });
+
     return io;
 }
+
+
+  
 
 function getIo() {
     if (!io) {
